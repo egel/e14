6d35ae668c186e225cb76baa5f1d81eae43d6edd
Maciej Sypien <maciejsypien@gmail.com>
2014-06-05 14:54:43 +0200

    Added Gallery class

--- strona/classes/Page.class.php
+++ strona/classes/Page.class.php
@@ -5,8 +5,7 @@ class Page
 
   // To są domyślne ustawienia zmiennych
   protected $_title      ="Not set yet";
-  protected $_header     ="Not set yet";
-  protected $_footer     ="Not set yet";
+  protected $_head       ="Not set yet";
   protected $_body       ="Not set yet";
   protected $_mainmenu   ="Not set yet";
 
@@ -38,71 +37,63 @@ class Page
     }
   }
 
-  public function get_header()
+  public function get_head()
   {
     return '
     <!DOCTYPE html>
-    <html lang="en">
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="'.$this->_meta_description.'">
+        <meta name="author" content="'.$this->_meta_author.'">
+        <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="'.$this->_meta_description.'">
-      <meta name="author" content="'.$this->_meta_author.'">
-      <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+        <title>'.$this->_title.'</title>
 
-      <title>'.$this->_title.'</title>
-
-      <!-- CSS -->
-      <link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
-    </head>
-    <body>
+        <!-- CSS -->
+        <link href="'.CSS_BASEDIR.'/styles.css" rel="stylesheet">
+      </head>
     ';
   }
 
   public function get_body()
   {
     $tresc = '
-    <div id="naglowek-strony">
-    </div>
+      <body>
+        <div id="naglowek-strony">
+        </div>
 
-    <div id="menu-nawigacyjne">
-    '.$this->_mainmenu.'
-    </div>
+        <div id="calosc">
+          <div id="menu-nawigacyjne">
+          '.$this->_mainmenu.'
+          </div>
 
-    <div id="tresc-strony">
-    '.$this->_body.'
-    </div>
+          <div id="tresc-strony">
+          '.$this->_body.'
+          </div>
 
-    <div id="nowosci">
-    </div>
+          <div id="nowosci"></div>
+        </div><!-- end calosc -->
 
-    <div id="pasek-statusu">
-    </div>
+        <div id="pasek-statusu">
+        </div>
+
+        <!-- Core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="'.JS_BASEDIR.'/scripts.js"></script>
+      </body>
+    </html>
     ';
 
     return $tresc;
   }
 
-  public function get_footer()
-  {
-    return '
-        <!-- Core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      </body>
-    </html>
-    ';
-  }
-
-
   public function render_page()
   {
-    echo $this->get_header();
+    echo $this->get_head();
     echo $this->get_body();
-    echo $this->get_footer();
   }
 }
 
